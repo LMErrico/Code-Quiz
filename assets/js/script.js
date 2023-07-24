eb0c9677d49fb70fa5be0274b69f4a4ee5161903
Lima <linam.errico@gmail.com>
2023-07-24 16:26:44 -0400

    starter code and quiz structure

---
+++ assets/js/script.js
@@ -0,0 +1,181 @@
+var viewHighscores = document.querySelector(".view-highscores");
+var timerElement = document.querySelector(".timer-count");
+var startButton = document.querySelector(".start-button");
+
+var isWin = false;
+var timer;
+var timerCount;
+var questionsAnswered = 0;
+
+startButton.addEventListener("click", startGame);
+
+function startGame() {
+    isWin = false;
+    timerCount = 20;
+    startButton.disabled = true;
+    startTimer()
+    showQuestion()
+}
+
+// function startTimer() {
+//     timer = setInterval(function() {
+//       timerCount--;
+//       timerElement.textContent = timerCount;
+//       if (timerCount >= 0) {
+//         // Tests if win condition is met
+//         if (isWin && timerCount > 0) {
+//           // Clears interval and stops timer
+//           clearInterval(timer);
+//           winGame();
+//         }
+//       }
+//       // Tests if time has run out
+//       if (timerCount === 0) {
+//         // Clears interval
+//         clearInterval(timer);
+//         loseGame();
+//       }
+//     }, 1000);
+// }
+
+function startTimer() {
+  var timeInterval = setInterval(() => {
+    if (timerCount < 0) {
+      console.log("stop");
+      clearInterval(timeInterval);
+      isWin = false;
+      showFinalScore();
+      return;
+    }
+    timerElement.textContent = timerCount; 
+    timerCount--;
+  }, 1000);
+}
+
+const quizData = [
+    {
+      question: "Commonly used data types DO not include:",
+      choices: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
+      correctAnswer: "3. alerts"
+    },
+    {
+      question: "The condition in an if / else statement is enclosed with ______",
+      choices: ["1. quotes", "2. curly brackets", "3. parenthesis", "4. square brackets"],
+      correctAnswer: "3. parenthesis"
+    },
+    {
+      question: "Arrays in JavaScript can be used to store",
+      choices: ["1. numbers and strings", "2. other arrays", "3. booleans", "4. all of the above"],
+      correctAnswer: "4. all of the above"
+    },
+    {
+      question: "String values must be enclosed within _____ when being assigned to variables.",
+      choices: ["1. commas", "2. curly brackets", "3. quotes", "4. parenthesis"],
+      correctAnswer: "3. quotes"
+    }
+    
+  ];
+  
+  let currentQuestionIndex = 0;
+  console.log(currentQuestionIndex)
+  let score = 0;
+  
+  const questionElement = document.getElementById("question");
+  const choicesElement = document.getElementById("choices");
+  const resultElement = document.getElementById("result");
+  const scoreElement = document.getElementById("score");
+  
+  function showQuestion() {
+    const currentQuestion = quizData[currentQuestionIndex];
+    questionElement.textContent = currentQuestion.question;
+    choicesElement.innerHTML = "";
+  
+    currentQuestion.choices.forEach((choice) => {
+      const choiceButton = document.createElement("button");
+      choiceButton.textContent = choice;
+      choiceButton.addEventListener("click", () => checkAnswer(choice));
+      choicesElement.appendChild(choiceButton);
+    });
+  }
+  
+//   function checkAnswer(userChoice) { 
+//     // console.log(userChoice)
+//     const currentQuestion = quizData[currentQuestionIndex];
+//     if (userChoice === currentQuestion.correctAnswer) {
+//       score++;
+//       resultElement.textContent = "Correct!";  
+//     }
+  
+//     if (userChoice != currentQuestion.correctAnswer) {
+//         // console.log("wrong")
+//         timerCount -=10;
+//         resultElement.textContent = "Wrong!"  
+//     }
+
+//     currentQuestionIndex++;
+  
+//     if (currentQuestionIndex < quizData.length) {
+//       showQuestion();
+//     } else {
+//       showFinalScore();
+//     }
+//   }
+
+function checkAnswer(userChoice) { 
+    // console.log(userChoice)
+    const currentQuestion = quizData[currentQuestionIndex];
+    if (userChoice === currentQuestion.correctAnswer) {
+      score++;
+      resultElement.textContent = "Correct!";  
+    }
+  
+    if (userChoice != currentQuestion.correctAnswer) {
+        // console.log("wrong")
+        timerCount -=10;
+        resultElement.textContent = "Wrong!"  
+    }
+    questionsAnswered++
+    if (questionsAnswered == quizData.length) {
+        showFinalScore();
+    }
+
+    currentQuestionIndex++;
+  
+    if (currentQuestionIndex < quizData.length) {
+      showQuestion();
+    } else {
+      showFinalScore();
+    }
+  }
+
+  
+  function showFinalScore() {
+    // if (isWin = true)
+    questionElement.textContent = "GAME OVER!";
+    choicesElement.innerHTML = "";
+    startButton.style.display = "none";
+    scoreElement.textContent = `Final Score: ${score} of ${quizData.length}`;
+    localStorage.setItem("score", score)
+  }
+
+
+
+
+
+//   function showFinalScore() {
+//     if (isWin === true) {
+//     questionElement.textContent = "You win!";
+//   } else {
+//     questionElement.textContent = "You suck!";
+//   }
+//     choicesElement.innerHTML = "";
+//     startButton.style.display = "none";
+//     scoreElement.textContent = `Final Score: ${score} of ${quizData.length}`;
+//     localStorage.setItem("score", score)
+//   }
+
+ /* submitButton.addEventListener("click", showQuestion);*/
+  
+  
+//   startGame();*/
+
